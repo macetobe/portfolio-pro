48b4d6bda23ecaa927c9ad2b2452b8ace584f600
tobe <macetobe1996@gmail.com>
2020-06-18 19:39:55 +0100

    added boostrap and finished job app

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,7 @@
 from django.db import models
+
+class blog(models.Model):
+    title=models.CharField(max_length=240)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
